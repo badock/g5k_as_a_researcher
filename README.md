c0f9d6fb1cc9bc89231c8bd90c99f3f5215a48f7
Jonathan <jonathan.pastor@me.com>
2015-05-26 18:39:30 +0200

    use a classical pythonic project structure

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # g5k_as_a_researcher
 
 worker_0 => alebre
+
 worker_1 => badock
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='g5k_as_a_researcher',
+    version='0.1',
+    packages=['lib', 'lib.scholar'],
+    url='https://github.com/badock/g5k_as_a_researcher',
+    license='',
+    author='jonathan',
+    author_email='',
+    description=''
+)
